6cc6b1cb44ee24d6ddc0ccd5880a329d4336ec3c
Abdullateef01 <110469335+Abdullateef01@users.noreply.github.com>
2024-01-20 20:03:32 +0100

    Todo
    BootCamp24 assessment project

---
+++ style.js
@@ -0,0 +1,11 @@
+var chek = document.querySelector(".cross")
+
+function check() {
+    chek.classList.toggle("cross-maxwidth")
+}
+
+var chekk = document.querySelector(".crosstwo")
+
+function checktwo() {
+    chekk.classList.toggle("cross-maxwidths")
+}
